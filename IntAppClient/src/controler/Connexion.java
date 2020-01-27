23662208439e4be6e539909222cdc33c3bd9582f
Yanis <yanis.benin@gmail.com>
2020-01-26 21:15:54 -0400

    Connexion jsp ok

---
+++ IntAppClient/src/controler/Connexion.java
@@ -0,0 +1,52 @@
+package controler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Utilisateur;
+import model.UtilisateurProxy;
+
+/**
+ * Servlet implementation class Connexion
+ */
+@WebServlet("/Connexion")
+public class Connexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Connexion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		this.getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String pseudo = request.getParameter("pseudo");
+		String pswd = request.getParameter("pswd");
+		
+		boolean connect;
+		
+		Utilisateur service = new UtilisateurProxy();
+		connect = service.connecUsers(pseudo, pswd);
+		doGet(request, response);
+	}
+
+}
