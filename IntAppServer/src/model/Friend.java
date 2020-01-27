c564ced2755d3add7253f8db0fb1ace2c460cc1c
antoine <43969470+lightislight@users.noreply.github.com>
2020-01-27 03:31:32 +0100

    ajout de la liste d'ami

--- IntAppServer/src/model/Friend.java
+++ IntAppServer/src/model/Friend.java
@@ -5,5 +5,16 @@ public class Friend {
 	String nom;
 	String prenom;
 	String pseudo;
+	public Friend() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Friend(int id, String nom, String prenom, String pseudo) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.pseudo = pseudo;
+	}
 	
 }
